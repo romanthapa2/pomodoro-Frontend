1376e9793a20c48d61b77781b5d2aa69ae440738
roman thapa <thaparoman970@gmail.com>
2024-07-18 17:13:18 +0545

    update :  redux logic

--- src/components/navbar/settings/SettingContent.tsx
+++ src/components/navbar/settings/SettingContent.tsx
@@ -1,13 +1,34 @@
 import { useState,ChangeEvent } from "react";
+import { useDispatch } from "react-redux";
+import {setpomodoroTime,setLongBreak,setShortBreak} from "../../../reduxstore/TimeSlice"
 
-{
-  /* <input min="1" max="100" type="number" value="1"></input> */
-}
+
 const SettingContent = () => {
-  const[pomodoroTime,setPomodoroTime]=useState<number>(1);
-  const handleChange= (e: ChangeEvent<HTMLInputElement>) => {
-    setPomodoroTime(parseInt(e.target.value,10));
+  const dispatch = useDispatch();
+  const [pomodoroTime, setPomodoroTime] = useState<number>(25);
+  const [shortBreakTime, setShortBreakTime] = useState<number>(5);
+  const [longBreakTime, setLongBreakTime] = useState<number>(15);
+
+  const handlePomodoroChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value, 10);
+    setPomodoroTime(value);
+    dispatch(setpomodoroTime(value));
   };
+
+  const handleShortBreakChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value, 10);
+    setShortBreakTime(value);
+    dispatch(setShortBreak(value));
+  };
+
+  const handleLongBreakChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value, 10);
+    setLongBreakTime(value);
+    dispatch(setLongBreak(value));
+  };
+
+  
+  
   return (
     <div>
       <header className="h-8 border-b-2 flex justify-center">SETTING</header>
@@ -16,15 +37,19 @@ const SettingContent = () => {
       <div className="flex flex-row justify-start space-x-12 w-full">
         <div>
           <h3>Pomodoro</h3>
-          <input className="bg-slate-300 p-2 rounded-md" min="1" max="100" type="number" value={pomodoroTime} onChange={handleChange}></input>
+          <input className="bg-slate-300 p-2 rounded-md" min="1" max="100" type="number" value={pomodoroTime} onChange={handlePomodoroChange}></input>
         </div>
         <div>
           <h3>Short Break</h3>
-          <input min="1" max="100" type="number" value="1"></input>
+          <input min="1" max="100" type="number" value={shortBreakTime}
+          onChange={handleShortBreakChange}
+          ></input>
         </div>
         <div>
           <h3>Long Break</h3>
-          <input min="1" max="100" type="number" value="1"></input>
+          <input min="1" max="100" type="number" value={longBreakTime}
+          onChange={handleLongBreakChange}
+          ></input>
         </div>
       </div>
     </div>
--- src/reduxstore/TimeSlice.ts
+++ src/reduxstore/TimeSlice.ts
@@ -1,17 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initalstate = {
-
+interface TimeState {
+  pomodoroTime: number | null;
+  shortBreak: number | null;
+  longBreak: number | null;
 }
 
-const timeSlice = createSlice({
-    name: "time",
-    initialState: initalstate,
-    reducers: {
-        startTimer: (state) => {
-            // Start the timer
-        }
-    }
-})
+const initalstate: TimeState = {
+  pomodoroTime: 25,
+  shortBreak: 5,
+  longBreak: 15,
+};
 
-export default timeSlice.reducer
+const timeSlice = createSlice({
+  name: "time",
+  initialState: initalstate,
+  reducers: {
+    setpomodoroTime: (state, action: PayloadAction<number>) => {
+      state.pomodoroTime = action.payload;
+    },
+    setShortBreak: (state, action: PayloadAction<number>) => {
+      state.shortBreak = action.payload;
+    },
+    setLongBreak: (state, action: PayloadAction<number>) => {
+      state.longBreak = action.payload;
+    },
+  },
+});
+
+export const { setpomodoroTime,setShortBreak,setLongBreak } = timeSlice.actions;
+export default timeSlice.reducer;
